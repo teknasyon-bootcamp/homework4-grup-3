772eb34aca483de5ed1fa9a17369989dcd3f0d35
unknown <hasannolur06@gmail.com>
2021-09-08 15:46:59 +0300

    index.php template hazırlık

--- template/index.php
+++ template/index.php
@@ -7,55 +7,95 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
     <title>Postlarımız</title>
+
+
+    <script src="https://kit.fontawesome.com/c400c00162.js" crossorigin="anonymous"></script>
+
   </head>
   <body>
 
-
     <div class="container">
-      <div class="row">
-        <div class="col-12">
 
-          <!-- post card yeri -->
+        <div class="row text-success text-center">
+          <h1>- Postlarımız -</h1>
+        </div>
 
-          <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Rukiye</h5>
-              <p class="card-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque assumenda doloremque, blanditiis quaerat fuga laboriosam in alias maiores nam dolore quis sequi iusto dolorum modi corporis nostrum, asperiores voluptatum delectus.
-              </p>
-              <a href="#" class="btn btn-outline-info">Detay</a>
-            </div>
+        <div class="row justify-content-evenly">
+
+        <!-- Post için hazırladığımız kardlar -->
+
+
+        <div class="card mt-5" style="width: 18rem;">
+          <img src="../config/images/wall1.jpg" class="card-img-top mt-2" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Rukiye Çadırcı</h5>
+            <p class="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione dignissimos adipisci aperiam quas nesciunt amet quos asperiores repellat ipsam consequatur facilis deserunt quasi omnis non, quisquam provident voluptate aliquam minus!
+            </p>
+            <a href="#" class="btn btn-outline-info">Detay <i class="fas fa-pen"></i></a>
           </div>
+        </div>
 
 
+        <div class="card mt-5" style="width: 18rem;">
+          <img src="../config/images/wall1.jpg" class="card-img-top mt-2" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Rukiye Çadırcı</h5>
+            <p class="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione dignissimos adipisci aperiam quas nesciunt amet quos asperiores repellat ipsam consequatur facilis deserunt quasi omnis non, quisquam provident voluptate aliquam minus!
+            </p>
+            <a href="#" class="btn btn-outline-info">Detay <i class="fas fa-pen"></i></a>
+          </div>
+        </div>
+
+        <div class="card mt-5" style="width: 18rem;">
+          <img src="../config/images/wall1.jpg" class="card-img-top mt-2" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Rukiye Çadırcı</h5>
+            <p class="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione dignissimos adipisci aperiam quas nesciunt amet quos asperiores repellat ipsam consequatur facilis deserunt quasi omnis non, quisquam provident voluptate aliquam minus!
+            </p>
+            <a href="#" class="btn btn-outline-info">Detay <i class="fas fa-pen"></i></a>
+          </div>
+        </div>
 
 
+        <div class="card mt-5" style="width: 18rem;">
+          <img src="../config/images/wall1.jpg" class="card-img-top mt-2" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Rukiye Çadırcı</h5>
+            <p class="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione dignissimos adipisci aperiam quas nesciunt amet quos asperiores repellat ipsam consequatur facilis deserunt quasi omnis non, quisquam provident voluptate aliquam minus!
+            </p>
+            <a href="#" class="btn btn-outline-info">Detay <i class="fas fa-pen"></i></a>
+          </div>
+        </div>
+
+        <div class="card mt-5" style="width: 18rem;">
+          <img src="../config/images/wall1.jpg" class="card-img-top mt-2" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Rukiye Çadırcı</h5>
+            <p class="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione dignissimos adipisci aperiam quas nesciunt amet quos asperiores repellat ipsam consequatur facilis deserunt quasi omnis non, quisquam provident voluptate aliquam minus!
+            </p>
+            <a href="#" class="btn btn-outline-info">Detay <i class="fas fa-pen"></i></a>
+          </div>
+        </div>
+
+       
+        
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        <!-- Post için hazırladığımız kardların bitişi -->
 
 
         </div>
-      </div>
+
+
     </div>
 
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+  </body>
 </html>
